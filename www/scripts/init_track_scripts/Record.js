bd7d6b48056a923ebeaa783c7a4ef69d5edab4e0
Piero <piero@prehnit.hr>
2021-02-20 23:48:11 +0100

    + added more functionalities to initiative tracker

--- www/scripts/init_track_scripts/Record.js
+++ www/scripts/init_track_scripts/Record.js
@@ -1,56 +1,102 @@
-
 define([], function() {
 
 	class Record {
-		constructor(className, name, initValue, initBonus, onChangeCallback, handleAdd) {
+		constructor(className, name, isPlayer, initValue, initBonus, onChangeCallback) {
 			this.name = name;
 			this.initValue = initValue;
 			this.initBonus = initBonus;
 			this.className = className;
 			this.onChangeCallback = onChangeCallback;
+			this.isPlayer = isPlayer;
 		}
-
 
 		/**
 			Event listeners for input elements in a record
 			*/
 
-		handleSave(a, b) {
-			console.log("save", a, b)
-		} 
+		changeInitValue(evt) {
+			evt.preventDefault();
+			const value = evt.target.value;
+			if (isNotNum(value, -1000, 1000)) {
+				this.initValue = 0;
+			} else {
+				this.initValue = parseInt(value, 10);
+			}
 
-		changeInitValue(a, b, c) {
-			console.log(a, b, c);
-			console.log(this.className);
+			const initValueEl = document.getElementById(`${this.className}-init-value`);
+			initValueEl.value = this.initValue;
+
+			this.changeInitLabel.bind(this)();
+			this.onChangeCallback();
+		}
+
+		changeInitBonus(evt, buttonChange) {
+			const value = evt === null ? (parseInt(document.getElementById(`${this.className}-init-bonus`).value, 10) + buttonChange) : evt.target.value;
+			if (isNotNum(value, -1000, 1000)) {
+				this.initBonus = 0;
+			} else {
+				this.initBonus = parseInt(value, 10);
+			}
+
+			const initBonusEl = document.getElementById(`${this.className}-init-bonus`);
+			initBonusEl.value = this.initBonus;
+
+			this.changeInitLabel.bind(this)();
+			this.onChangeCallback();
+		}
+
+		changeInitLabel() {
+			const initValueEl = document.getElementById(`${this.className}-init-value`);
+			this.initValue = parseInt(initValueEl.value, 10);
+			const initBonusEl = document.getElementById(`${this.className}-init-bonus`);
+			this.initBonus = parseInt(initBonusEl.value, 10);
+
+			const initLabelEl = document.getElementById(`${this.className}-init-result`);
+			initLabelEl.innerText = parseInt(this.initValue, 10) + parseInt(this.initBonus, 10);
+		}
+
+		changeName(evt) {
+			evt.preventDefault();
+			this.name = evt.target.value;
+		}
+
+		handleDelete() {
+			const thisEl = document.getElementById(this.className);
+			thisEl.remove();
 		}
 
 		/**
 			Create all elements in a record
 		  */
+
 		createInitBonus() {
 			const divInitBonus = document.createElement("div");
 			divInitBonus.classList.add("init-bonus");
-			divInitBonus.classList.add("input-wrapper");
 
 			const decBtn = document.createElement("button");
 			decBtn.classList.add("init-btn");
-			decBtn.addEventListener("click", () => this.changeInitBonus(null, this.initBonus - 1));
+			decBtn.setAttribute("tabindex","-1")
+			decBtn.addEventListener("click", () => this.changeInitBonus.bind(this)(null, -1));
+
 			const decImg = document.createElement("img");
 			decImg.setAttribute("src", "../images/chevron-left-solid.svg");
 			decBtn.appendChild(decImg)
 
 			const incBtn = document.createElement("button");
 			incBtn.classList.add("init-btn");
-			incBtn.addEventListener("click", () => this.changeInitBonus(null, this.initBonus + 1));
+			incBtn.setAttribute("tabindex","-1")
+			incBtn.addEventListener("click", () => this.changeInitBonus.bind(this)(null, 1));
+
 			const incImg = document.createElement("img");
 			incImg.setAttribute("src", "../images/chevron-right-solid.svg");
 			incBtn.appendChild(incImg)
 
 			const inputNum = document.createElement("input");
-			inputNum.classList.add(`${this.className}-init-bonus`);
+			inputNum.classList.add("is-num");
+			inputNum.setAttribute("id", `${this.className}-init-bonus`);
 			inputNum.setAttribute("type", "text");
 			inputNum.setAttribute("value", this.initBonus);
-			inputNum.addEventListener("change", this.changeInitBonus);
+			inputNum.addEventListener("change", this.changeInitBonus.bind(this));
 
 			divInitBonus.appendChild(decBtn);
 			divInitBonus.appendChild(inputNum);
@@ -64,12 +110,34 @@ define([], function() {
 			divInitValue.classList.add("input-wrapper");
 
 			const inputNum = document.createElement("input");
-			inputNum.classList.add(`${this.className}-init-value`);
+			inputNum.classList.add("init-value-input");
+			inputNum.classList.add("is-num");
+			inputNum.setAttribute("id", `${this.className}-init-value`);
 			inputNum.setAttribute("type", "text");
-			inputNum.setAttribute("value", this.initValue);
-			inputNum.addEventListener("change", this.changeInitValue);
+			inputNum.setAttribute("value", `${this.initValue}`);
+			inputNum.addEventListener("change", this.changeInitValue.bind(this));
+
+			const plusLabel = document.createElement("label");
+			plusLabel.classList.add("init-label");
+			plusLabel.innerText = "+";
+
+			const initBonus = this.createInitBonus();
+
+			const eqLabel = document.createElement("label");
+			eqLabel.classList.add("init-label");
+			eqLabel.innerText = "=";
+
+			const resLabel = document.createElement("label");
+			resLabel.innerText = `${this.initValue + this.initBonus}`;
+			resLabel.classList.add("init-label");
+			resLabel.classList.add("init-label-result");
+			resLabel.setAttribute("id", `${this.className}-init-result`);
 
 			divInitValue.appendChild(inputNum);
+			divInitValue.appendChild(plusLabel);
+			divInitValue.appendChild(initBonus);
+			divInitValue.appendChild(eqLabel);
+			divInitValue.appendChild(resLabel);
 			return divInitValue;	
 		}
 
@@ -88,54 +156,60 @@ define([], function() {
 			return divName;
 		}
 
-		createEditBtn() {
-			const btn = document.createElement("button");
-			btn.classList.add("record-action")
-			const img = document.createElement("img");
-			img.setAttribute("src", "../images/edit-regular.svg");
-			btn.appendChild(img);
-			btn.addEventListener("click", this.handleEdit);
-			return btn;
+		createIsPlayerCBox() {
+			const divCBox = document.createElement("div");
+			divCBox.classList.add("input-wrapper-noborder");
+			divCBox.setAttribute("title", "Is player?");
+
+			const cBox = document.createElement("input");
+			cBox.setAttribute("type", "checkbox");
+			cBox.setAttribute("id", `${this.className}-cbox`);
+			//cBox.setAttribute("checked", this.isPlayer);
+			cBox.checked = this.isPlayer
+
+			divCBox.appendChild(cBox);
+			return divCBox
 		}
 
-		createSaveBtn() {
+		createDeleteBtn() {
 			const btn = document.createElement("button");
-			btn.classList.add("record-action")
+			btn.classList.add("record-action");
+			btn.setAttribute("tabindex", "-1");
+
 			const img = document.createElement("img");
-			img.setAttribute("src", "../images/save-regular.svg");
+			img.setAttribute("src", "../images/trash-alt-regular.svg");
+
 			btn.appendChild(img);
-			btn.addEventListener("click", this.handleSave);
-			return btn;
+			btn.addEventListener("click", this.handleDelete.bind(this));
+			return btn;	
 		}
 
 		createActions() {
 			const actionDiv = document.createElement("div");
 			actionDiv.classList.add("record-actions");
-			const editBtn = this.createEditBtn();
-			const saveBtn = this.createSaveBtn();
-			actionDiv.appendChild(editBtn);
-			actionDiv.appendChild(saveBtn);
+
+			const deleteBtn = this.createDeleteBtn();
+			actionDiv.appendChild(deleteBtn)
+
 			return actionDiv;
 		}
 
 		createPlayer() {
 			const pDiv = document.createElement("div");
+			pDiv.setAttribute("id", `${this.className}`)
 			pDiv.classList.add("record");
+
+			const isPlayer = this.createIsPlayerCBox();
 			const name = this.createName();
 			const initValue = this.createInitValue();
-			const initBonus = this.createInitBonus();
-			//const actions = this.createActions();
+			const actions = this.createActions();
 
+			pDiv.appendChild(isPlayer);
 			pDiv.appendChild(name);
 			pDiv.appendChild(initValue);
-			pDiv.appendChild(initBonus);
-			//pDiv.appendChild(actions);
+			pDiv.appendChild(actions);
 
 			return pDiv
-		}
-
-		static asd() {
-			console.log("test")
 		}
 	}
 
--- www/scripts/inittrack.js
+++ www/scripts/inittrack.js
@@ -1,25 +1,94 @@
-const isNotNum = (n) => {
-	const intN = parseInt(n, 10);
-	return Object.is(intN, NaN) || intN >= 10 || intN <= 0;
-}
-
+var cnt = 10000;
 
 require(["../scripts/init_track_scripts/Record"], function(Record) {
-	let playerNum = prompt("Enter the number of players (less than 10)", 4);
 
-	if (isNotNum(playerNum)) {
-		while (isNotNum(playerNum = prompt("Enter the NUMBER of players (less than 10)", 4)));
+	// sort the records by initiative score
+	const sortRecords = (force = false) => {
+		const autoSort = document.getElementById("auto-sort");
+		if (!autoSort.checked && !force) {
+			return;
+		}
+
+
+		const initTrack = document.getElementsByClassName("init-track")[0];
+		const childrenArr = [...initTrack.children];
+		const sortedArr = childrenArr.sort((fstEl, sndEl) => {
+			const fId = fstEl.id;
+			const sId = sndEl.id;
+
+			const fVal = parseInt(document.getElementById(`${fId}-init-result`).innerText, 10);
+			const sVal = parseInt(document.getElementById(`${sId}-init-result`).innerText, 10);
+
+			return fVal > sVal ? -1 : fVal < sVal ? 1 : 0;
+		});
+
+		while (initTrack.lastChild) {
+			initTrack.lastChild.remove();
+		}
+
+		sortedArr.forEach(c => {
+			initTrack.appendChild(c);
+		});
 	}
 
+	const playerNum = 4;
 	const initTrack = document.getElementsByClassName("init-track")[0];
-
 	for (let i = 0; i < playerNum; ++i) {
-		const record = new Record(`record-${i}`, "Name", 0, 0, () => {}, () => {});
+		const record = new Record(`record-${i}`, i === 0 ? "" : "Name", true, 0, 0, sortRecords);
 		const currPlayer = record.createPlayer();
 		initTrack.appendChild(currPlayer);
 	}
-	
-	//const addBtn = create.addBtn();
-	//initTrack.appendChild(addBtn);
+	sortRecords(true);
+	const nameEl = document.getElementsByClassName(`record-0-name`)[0]
+	nameEl.focus();
+
+	const addRecordBtn = document.getElementsByClassName("add-record-btn")[0];
+	addRecordBtn.addEventListener("click", () => {
+		const initTrack = document.getElementsByClassName("init-track")[0];
+		const recordNum = document.getElementById("add-record-num");
+		const value = isNotNum(recordNum.value, 0, 30) ? 0 : parseInt(recordNum.value);
+		console.log(recordNum.value)
+		recordNum.value = value;
+		for (let i = 0; i < value; ++i) {
+			const newRecord = new Record(`record-${cnt++}`, "", false, 0, 0, sortRecords);
+			initTrack.appendChild(newRecord.createPlayer())
+			if (i === 0) {
+				const nameEl = document.getElementsByClassName(`record-${cnt - 1}-name`)[0]
+				nameEl.focus();
+			}
+		}
+	});
+
+	const autoSort = document.getElementById("auto-sort");
+	autoSort.addEventListener("change", function(evt) {
+		const sortBtn = document.getElementById("do-sort");
+		sortBtn.disabled = evt.target.checked
+	});
+
+	const sortBtn = document.getElementById("do-sort");
+	sortBtn.addEventListener("click", () => sortRecords(true));
+
+	const rollBtn = document.getElementById("do-roll");
+	rollBtn.addEventListener("click", () => {
+		const initTrack = document.getElementsByClassName("init-track")[0];
+		const childrenArr = [...initTrack.children];
+		childrenArr.forEach(c => {
+			const id = c.id;
+			const isPlayer = document.getElementById(`${id}-cbox`);
+			if (!isPlayer.checked) {
+				const initValueEl = document.getElementById(`${id}-init-value`);
+				const initVal = 5 + Math.ceil(100 * Math.random()) % 15;
+				initValueEl.value = initVal;
+
+				const initBonusEl = document.getElementById(`${id}-init-bonus`);
+				const initBonus = initBonusEl.value;
+
+				const initResEl = document.getElementById(`${id}-init-result`);
+				initResEl.innerText = parseInt(initVal, 10) + parseInt(initBonus, 10);
+			}
+		})
+		sortRecords(true);				
+	})
+
 });
 
